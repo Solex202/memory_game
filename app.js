0068230ce566d97dcd9c3f4c4750a4e7f5640820
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-07-02 14:00:37 +0100

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const cardArray = [
     {
         name: 'fries',
-        img: ''
+        img: 'images/fries.png'
     },
     {
         name: 'cheeseburger',
@@ -52,15 +52,48 @@ const cardArray = [
 cardArray.sort(() =>0.5 - Math.random())
 
 const gridDisplay = document.querySelector('#grid')
+let cardChosen = []
+let cardChosenIds = []
+const cardWon = []
 
 function createBoard(){
     for(let i = 0; i < cardArray.length; i++){
         const card = document.createElement('img')
-        // console.log(card, i);
         card.setAttribute('src', 'images/blank.png')
         card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
         gridDisplay.appendChild(card)
+        console.log(card.img);
     }
 }
 
 createBoard()
+
+function checkMatch(){
+
+const cards = document.querySelectorAll('img')
+console.log(cards);
+    // if()
+    if(cardChosen[0] == cardChosen[1]){
+        alert('you found a match')
+        cards[cardChosenIds[0]].setAttribute('src', 'images/white.png')
+        cards[cardChosenIds[1]].setAttribute('src', 'images/white.png')
+        cards[cardChosenIds[0]].removeEventListener('click', flipCard)
+        cards[cardChosenIds[1]].removeEventListener('click', flipCard)
+        cardWon.push(cardChosen)
+    }
+    cardChosen = []
+    cardChosenIds = []
+}
+
+function flipCard(){
+    console.log(cardArray);
+    const cardId = this.getAttribute('data-id')
+    cardChosen.push(cardArray[cardId].name)
+    cardChosenIds.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+
+    if(cardChosen.length === 2){
+        setTimeout(checkMatch, 500)
+    }
+}
